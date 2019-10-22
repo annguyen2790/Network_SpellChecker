04e9be92084121cf7587fa9d68990db0cda9e0af
An Nguyen <tuh09895@temple.edu>
2019-10-21 23:36:19 -0400

    Added buffers and other components for the server

--- spchk_header.h
+++ spchk_header.h
@@ -3,6 +3,24 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#define QUEUE_SIZE_DEFAULT 20
 int accept_connection(int port_number);
 char ** load_dictionary(char * input_file);
+
+typedef struct{
+  pthread_mutex_t lock;
+  pthread_cond_t empty_check;
+  pthread_cond_t fill_check;
+  int client_num;
+  int clients_in_socket[QUEUE_SIZE_DEFAULT];
+
+}clientsQueue;
+
+typedef struct{
+  pthread_mutex_t lock;
+  pthread_cond_t empty_check;
+  pthread_cond_t fill_check;
+  char log [QUEUE_SIZE_DEFAULT][300];
+  int log_num;
+
+}logsQueue;
